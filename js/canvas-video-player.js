464cc624f208e228b3be7038b09347b1165c08f1
simon <simon.neyt@student.howest.be>
2019-06-06 18:08:01 +0200

    Initial commit

---
+++ js/canvas-video-player.js
@@ -0,0 +1,23 @@
+
+
+var isIOS = /iPad|iPhone|iPod/.test(navigator.platform);
+
+if (isIOS) {
+
+        canvasVideo = new CanvasVideoPlayer({
+        videoSelector: '.video',
+        canvasSelector: '.canvas',
+        timelineSelector: false,
+        autoplay: true,
+        makeLoop: true,
+        pauseOnClick: false,
+        audio: false
+    })
+
+}else {
+
+    // Use HTML5 video
+    document.querySelectorAll('.canvas')[0].style.display = 'none'
+
+}   
+
